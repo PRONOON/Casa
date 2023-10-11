0d344a18b12af4c84335ec572b5ed7c4c49711ee
PRONOON <131971835+PRONOON@users.noreply.github.com>
2023-10-11 06:49:53 +0100

    Update validation.js

--- validation.js
+++ validation.js
@@ -6,7 +6,7 @@ function checkValidationStatus() {
     if (!validationStatus || !JSON.parse(validationStatus).validated) {
         // Validation status not found or not validated
         alert("VALIDATION REQUIRED: This usually happens when you access our platform on a new phone or browser.");
-        window.location.href = "VERIFY.html"; // Redirect to OTP page
+        window.location.href = "verify.html"; // Redirect to phone number input page
     }
 }
 
